f3e439aff8ac0049790d971edc17fc00e6e9203f
Sam Hosseini <git-email@hosseini.io>
2021-05-12 17:31:17 +0300

    add PARKING_PERMITS_GRAPHQL_API env var and wait for it to initialize

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,6 +2,11 @@
 
 set -e
 
+if [[ "$WAIT_FOR_PARKING_PERMITS_GRAPHQL_API" = "true" ]]; then
+    ADDRESS=$(echo $PARKING_PERMITS_GRAPHQL_API | cut -c 8-)
+    ./wait-for-it.sh $ADDRESS --timeout=120
+fi
+
 if [[ "$DOCKER_BIND_MOUNT_IN_USE" = "true" && ! -d "/app/node_modules/" ]]; then
     yarn install --frozen-lockfile
 fi
