768540e77c8827f88cf8a12aea0b7faa34773aa5
Sam Hosseini <git-email@hosseini.io>
2021-06-10 17:46:44 +0300

    update PARKING_PERMITS_GRAPHQL_API value

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,7 +8,7 @@ if [[ "$WAIT_FOR_OPEN_CITY_PROFILE_GRAPHQL_API" = "true" ]]; then
 fi
 
 if [[ "$WAIT_FOR_PARKING_PERMITS_GRAPHQL_API" = "true" ]]; then
-    ADDRESS=$(echo $PARKING_PERMITS_GRAPHQL_API | cut -c 8-)
+    ADDRESS=$(echo $PARKING_PERMITS_GRAPHQL_API | cut -c 8-31)
     ./wait-for-it.sh $ADDRESS --timeout=120
 fi
 
