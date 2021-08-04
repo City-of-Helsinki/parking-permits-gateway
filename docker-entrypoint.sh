ef9e715d78facc61fcf34bc5de82e25dd4bfa4dc
Sam Hosseini <git-email@hosseini.io>
2021-08-04 13:45:51 +0300

    update OPEN_CITY_PROFILE_GRAPHQL_API env var to use staging environment

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,11 +2,6 @@
 
 set -e
 
-if [[ "$WAIT_FOR_OPEN_CITY_PROFILE_GRAPHQL_API" = "true" ]]; then
-    ADDRESS=$(echo $OPEN_CITY_PROFILE_GRAPHQL_API | cut -d '/' -f 3)
-    ./wait-for-it.sh $ADDRESS --timeout=120
-fi
-
 if [[ "$WAIT_FOR_PARKING_PERMITS_GRAPHQL_API" = "true" ]]; then
     ADDRESS=$(echo $PARKING_PERMITS_GRAPHQL_API | cut -d '/' -f 3)
     ./wait-for-it.sh $ADDRESS --timeout=120
